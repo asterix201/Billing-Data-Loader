855443529c2184eadeca1581484b165c412991fc
Gennadiy Khramov <gennadiy.khramov@decathlon.com>
2021-01-13 15:36:12 +0300

    working version with one-host clickhouse db

--- main.py
+++ main.py
@@ -7,46 +7,44 @@ from datetime import datetime
 from io import StringIO
 import os
 
-
 CH_PASSWORD = os.environ['CH_PASSWORD']
 CH_HOST = 'https://{host}:8443/?database={db}'.format(
-    host=os.environ['CH_HOST'],
-    db=os.environ['CH_DB'])
+        host= os.environ['CH_HOST'],
+        db= os.environ['CH_DB'])
 AUTH = {
-    'X-ClickHouse-User': os.environ['CH_USER'],
-    'X-ClickHouse-Key': CH_PASSWORD,
-}
+        'X-ClickHouse-User': os.environ['CH_USER'],
+        'X-ClickHouse-Key': CH_PASSWORD,
+       }
 CERT = './CA.pem'
 BUCKET = os.environ['STORAGE_BUCKET']
 FOLDER = os.environ['STORAGE_FOLDER']
 TABLE = os.environ['CH_TABLE']
+START_DATE = os.environ['START_DATE']
 columns = [
-    'billing_account_id',
-    'billing_account_name',
-    'cloud_id',
-    'cloud_name',
-    'folder_id',
-    'folder_name',
-    'resource_id',
-    'service_id',
-    'service_name',
-    'sku_id',
-    'sku_name',
-    'date',
-    'currency',
-    'pricing_quantity',
-    'pricing_unit',
-    'cost',
-    'credit',
-    'monetary_grant_credit',
-    'volume_incentive_credit',
-    'cud_credit',
-    'misc_credit',
-    'locale',
-    'updated_at',
-    'exported_at'
+            'billing_account_id',
+            'billing_account_name',
+            'cloud_id',
+            'cloud_name',
+            'cost',
+            'credit',
+            'cud_credit',
+            'currency',
+            'date',
+            'exported_at',
+            'folder_id',
+            'folder_name',
+            'locale',
+            'misc_credit',
+            'monetary_grant_credit',
+            'pricing_quantity',
+            'pricing_unit',
+            'service_id',
+            'service_name',
+            'sku_id',
+            'sku_name',
+            'updated_at',
+            'volume_incentive_credit'
 ]
-
 
 def request():
     url = (CH_HOST + '&query={query}').format(
@@ -86,18 +84,18 @@ def upload(table, content, host=CH_HOST):
     else:
         raise ValueError(r.text)
 
-
 # Shape Data
 def shape_df(tmp_df):
+
     shaped_df = pd.DataFrame(columns=columns)
     for col in columns:
         try:
-            shaped_df[col] = tmp_df[col]
+            shaped_df[col]=tmp_df[col]
         except KeyError as cerr:
             shaped_df[col] = ""
 
     shaped_df["date"] = pd.to_datetime(shaped_df["date"]).dt.round('D')
-    # tmp_df["exported_at"] = tmp_df["exported_at"].dt.round('s')
+    #tmp_df["exported_at"] = tmp_df["exported_at"].dt.round('s')
 
     decimal_columns: List[str] = [
         'pricing_quantity',
@@ -110,19 +108,19 @@ def shape_df(tmp_df):
     ]
 
     for col in decimal_columns:
-        shaped_df[col] = pd.to_numeric(shaped_df[col])
-        shaped_df[col] = shaped_df[col].round(10)
+            shaped_df[col] = pd.to_numeric(shaped_df[col])
+            shaped_df[col] = shaped_df[col].round(10)
 
     return shaped_df
 
-
 def init(drop=False):
+
     if (drop):
-        q = '''drop table if exists ''' + TABLE + ''' on cluster '{cluster}';'''
+        q='''drop table if exists '''  + TABLE
         get_clickhouse_data(q)
 
     q = '''
-        CREATE TABLE IF NOT EXISTS ''' + TABLE + ''' on cluster '{cluster}'
+        CREATE TABLE IF NOT EXISTS ''' + TABLE + '''
         (
             billing_account_id  String,
             billing_account_name   String,
@@ -130,7 +128,6 @@ def init(drop=False):
             cloud_name String, 
             folder_id String,
             folder_name    String,
-            resource_id String,
             service_id String,
             service_name String,   
             sku_id String,
@@ -149,12 +146,11 @@ def init(drop=False):
             updated_at  String,
             exported_at String
         )
-        ENGINE = ReplicatedMergeTree('/clickhouse/tables/{shard}/''' + TABLE + '''', '{replica}') 
-        PARTITION BY date 
-        ORDER BY (date, sku_id) 
+        ENGINE = MergeTree() 
+        PARTITION BY date
+        ORDER BY (date, sku_id)
         '''
     get_clickhouse_data(q)
-
 
 def clear_part(part_dt):
     q = '''ALTER TABLE ''' + TABLE + ''' DROP PARTITION ''' + pd.to_datetime(part_dt).strftime("'%Y-%m-%d'")
@@ -162,6 +158,7 @@ def clear_part(part_dt):
 
 
 def reload(event, context):
+
     init(True)
 
     # q = '''select concat(replace(toString(
@@ -171,16 +168,16 @@ def reload(event, context):
     #     start_key = get_clickhouse_data(q).rstrip()
     # except ValueError:
     #     start_key = '20180102.csv'
-    start_key = '20190101.csv'
+    start_key = START_DATE+'.csv'
     session = boto3.session.Session()
     s3 = session.client(
         service_name='s3',
         endpoint_url='https://storage.yandexcloud.net'
     )
 
-    kwargs = {"Bucket": BUCKET, "Prefix": FOLDER, "MaxKeys": 100, "StartAfter": FOLDER + '/' + start_key}
+    kwargs = {"Bucket": BUCKET, "Prefix" : FOLDER, "MaxKeys" : 100, "StartAfter" : FOLDER + '/' + start_key}
     continuation_token = None
-    bck_cnt = 0
+    bck_cnt=0
     while True:
         if continuation_token:
             kwargs['ContinuationToken'] = continuation_token
@@ -188,21 +185,20 @@ def reload(event, context):
         for key in obj_list['Contents']:
             try:
                 get_object_response = s3.get_object(Bucket=BUCKET, Key=key['Key'])
-                df = pd.read_csv(StringIO(get_object_response['Body'].read().decode('utf-8')),
-                                 usecols=lambda x: x in columns)
+                df = pd.read_csv(StringIO(get_object_response['Body'].read().decode('utf-8')))
                 df = shape_df(df)
                 for part_dt in df["date"].unique():
-                    clear_part(part_dt)
+                   clear_part(part_dt)
                 upload(
                     TABLE,
                     df.to_csv(index=False, sep='\t'))
-                print('object ' + key['Key'] + ' uploaded')
+                print('object '+ key['Key'] + ' uploaded' )
                 bck_cnt = bck_cnt + 1
             except KeyError as err:
-                print('object ' + key['Key'] + ' error: ' + str(err))
-                # print ('No objects found in Bucket ' + BUCKET + ' with prefix ' + FOLDER)
+                print ('object '+ key['Key'] + ' error: ' + str(err))
+                #print ('No objects found in Bucket ' + BUCKET + ' with prefix ' + FOLDER)
             except ValueError as err:
-                print('object ' + key['Key'] + ' error: ' + str(err))
+                print ('object '+ key['Key'] + ' error: ' + str(err))
         if not obj_list.get('IsTruncated'):  # At the end of the list?
             break
         continuation_token = obj_list.get('NextContinuationToken')
@@ -212,8 +208,8 @@ def reload(event, context):
         'isBase64Encoded': False,
     }
 
-
 def increment(event, context):
+
     init(False)
     session = boto3.session.Session()
     s3 = session.client(
@@ -226,7 +222,7 @@ def increment(event, context):
         cur_object = record['details']['object_id']
 
         get_object_response = s3.get_object(Bucket=cur_bucket, Key=cur_object)
-        df = pd.read_csv(StringIO(get_object_response['Body'].read().decode('utf-8')), usecols=lambda x: x in columns)
+        df = pd.read_csv(StringIO(get_object_response['Body'].read().decode('utf-8')))
         df = shape_df(df)
         for part_dt in df["date"].unique():
             clear_part(part_dt)
@@ -237,12 +233,11 @@ def increment(event, context):
 
     return {
         'statusCode': 200,
-        #  'body': str(bck_cnt) + ' objects loaded',
+      #  'body': str(bck_cnt) + ' objects loaded',
         'isBase64Encoded': False,
     }
-
-
 def handler(event, context):
+
     try:
         method = event['queryStringParameters']['method']
     except KeyError as err:
@@ -255,7 +250,7 @@ def handler(event, context):
         cur_bucket = event['messages'][0]['details']['bucket_id']
         cur_object = event['messages'][0]['details']['object_id']
     except KeyError as err:
-        print("Wrong function call")
+        print ("Wrong function call")
         return {
             'statusCode': 400,
             'body': 'Wrong function call - either pass method=reload as a parameter or use s3 trigger',
